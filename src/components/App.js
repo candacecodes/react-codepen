dd94690da0ddbb29218b07fe74e93e2405c29011
candacecodes <candacehchoe@gmail.com>
2020-10-15 14:27:33 -0700

    css and framework

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Editor from "./Editor";
+
+function App() {
+	const [html, setHtml] = useState("");
+	const [css, setCss] = useState("");
+	const [js, setJs] = useState("");
+
+	return (
+		<>
+			<div className="pane top-pane">
+				<Editor
+					language="xml"
+					displayName="HTML"
+					value={html}
+					onChange={setHtml}
+				/>
+				<Editor
+					language="xml"
+					displayName="HTML"
+					value={css}
+					onChange={setCss}
+				/>{" "}
+				<Editor language="xml" displayName="HTML" value={js} onChange={setJs} />
+			</div>
+			<div className="pane">
+				<iframe
+					title="output"
+					sandbox="allow-scripts"
+					frameBorder="0"
+					width="100%"
+					height="100%"
+				/>
+			</div>
+		</>
+	);
+}
+
+export default App;
