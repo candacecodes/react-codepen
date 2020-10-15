ab5d30882d88f25c2a2894946f6f778b3b0b5857
candacecodes <candacehchoe@gmail.com>
2020-10-15 15:13:27 -0700

    added feature to open and collapse editors

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
 
 function App() {
 	const [html, setHtml] = useState("");
 	const [css, setCss] = useState("");
 	const [js, setJs] = useState("");
+	const [srcDoc, setSrcDoc] = useState("");
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setSrcDoc(`
+			<html>
+			<body>${html}</body>
+			<style>${css}</style>
+			<script>${js}</script>
+		</html>`);
+		}, 250);
+
+		return () => clearTimeout(timeout);
+	}, [html, css, js]);
 
 	return (
 		<>
@@ -17,14 +31,15 @@ function App() {
 				/>
 				<Editor
 					language="xml"
-					displayName="HTML"
+					displayName="CSS"
 					value={css}
 					onChange={setCss}
 				/>{" "}
-				<Editor language="xml" displayName="HTML" value={js} onChange={setJs} />
+				<Editor language="xml" displayName="JS" value={js} onChange={setJs} />
 			</div>
 			<div className="pane">
 				<iframe
+					srcDoc={srcDoc}
 					title="output"
 					sandbox="allow-scripts"
 					frameBorder="0"
